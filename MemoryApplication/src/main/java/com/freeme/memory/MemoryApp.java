79cef9856e58844f7ce571b7ecb709cef8316c1a
kongdehui <kongdehui@droi.com>
2016-11-28 15:07:32 +0800

    add advertisement environment

--- MemoryApplication/src/main/java/com/freeme/memory/MemoryApp.java
+++ MemoryApplication/src/main/java/com/freeme/memory/MemoryApp.java
@@ -1,7 +1,9 @@
 package com.freeme.memory;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.droi.adcontrol.AdControl;
 import com.freeme.memories.base.AppImpl;
 import com.freeme.updateself.update.UpdateMonitor;
 
@@ -10,7 +12,7 @@ import com.freeme.updateself.update.UpdateMonitor;
  */
 public class MemoryApp extends Application {
 
-
+    private static final String TAG = "MemoryApp";
     @Override
     public void onCreate() {
         super.onCreate();
@@ -25,6 +27,15 @@ public class MemoryApp extends Application {
                 //*/ default notify small icon, ifnot set use updateself_ic_notify_small
 //                .setDefaultNotifyIcon(R.drawable.updateself_ic_notify_small)
                 .complete();
+        try {
+            Log.d(TAG,"application oncreate");
+            AdControl.Init(this)
+                    .sync();
+            AdControl.setDebugMode(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d(TAG,"sdk init error");
+        }
     }
 
     @Override
